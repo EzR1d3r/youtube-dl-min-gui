e302230e907d82ae27fbf127bc3b8574ed32825b
EzR1d3r <brd1080@yandex.ru>
2020-10-06 21:38:11 +0300

    Распределение функционала по файлам.

---
+++ main_window.py
@@ -0,0 +1,87 @@
+from threading import Thread
+import subprocess
+import os
+
+from tkinter import Tk, Label, Button, Entry, StringVar, Text
+from tkinter import LEFT, END
+
+import utils as ut
+
+title_scheme = "%(title)s.%(ext)s"
+rel_dl_path = os.path.join("~", "Downloads", "youtube-downloads", title_scheme)
+dl_path = os.path.expanduser( rel_dl_path )
+
+class MainWindow:
+    def __init__(self):
+        #tk gui root
+        self.root = Tk()
+        self.root.title("Youtube-dl")
+        self.root.minsize(720,360)
+        self.txtConsole = Text(bg="black", fg="#2bfe72")
+
+        #labels
+        self.lbLink = Label(self.root, text="Download link: ", justify=LEFT)
+        self.lbLink.grid(row=0,column=0,sticky="W",pady=10, padx=10)
+
+        self.lbOptions = Label(self.root, text="Options: ", justify=LEFT)
+        self.lbOptions.grid(row=1,column=0, sticky="W", pady=10, padx=10)
+
+        #entrys
+        self.entDwnLink = Entry(self.root)
+        self.entDwnLink.grid(row=0, column=1, sticky="WE", columnspan=2, pady=10, padx=10)
+        self.entDwnLink.insert(0, "https://www.youtube.com/watch?v=S6ygE226h-0")
+
+        self.entOptions = Entry(self.root, width=100)
+        self.entOptions.grid(row=1,column=1, sticky="WE", columnspan=2, pady=10, padx=10)
+        self.entOptions.insert(0, "-f 399+140")
+
+        #buttons
+        self.btnDownload = Button(self.root, text="Download", width=20)
+        self.btnDownload.grid(row=100, column=1, pady=10, padx=10, sticky="E")
+
+        self.btnInfo = Button(self.root, text="Info", width=20)
+        self.btnInfo.grid(row=100, column=2, pady=10, padx=10, sticky="E")
+
+        # text
+        self.txtConsole.grid(row=102, column=0, columnspan=3, sticky="WNES", pady=10, padx=10)
+
+        #root
+        self.root.columnconfigure(0, weight=0, minsize=100)
+        self.root.columnconfigure(1, weight=10, minsize=300)
+        self.root.rowconfigure(102, weight=100)
+
+        self.bind_gui()
+
+    def bind_gui(self):
+        self.entDwnLink.bind("<Button-3>", lambda x: self.entDwnLink.insert(0, self.entDwnLink.selection_get(selection='CLIPBOARD') ))
+        self.btnDownload.bind("<ButtonRelease>", lambda x: self.download(self.entDwnLink.get(), self.entOptions.get()))
+        self.btnInfo.bind("<ButtonRelease>", lambda x: self.get_info(self.entDwnLink.get()))
+
+    def mainloop(self):
+        self.root.mainloop()
+
+    def download(self, link, options_str):
+        options = options_str.split(" ") if options_str else []
+        options+=["-o", dl_path]
+        proc = ut.exec_youtube_dl(link, *options)
+        self.__redirect_out(proc)
+
+    def get_info(self, link):
+        proc = ut.exec_get_info(link)
+        self.__redirect_out(proc)
+        
+    def __redirect_out(self, proc: subprocess.Popen):
+        #read youtube-dl output and redirect to the console
+        t = Thread(target=ut.read_output, args=(proc,), kwargs={"out_append":self.append_console_line, "out_replace":self.replace_last_console_line})
+        t.start()
+
+    #work with console text
+    def append_console_line(self, text_line: str):
+        ut._append_text_item_text_line(self.txtConsole, text_line)
+        ut._post_format(self.txtConsole, text_line, contains={"mp4+1080p":"cyan", "m4a":"magenta"})
+
+    def clear_and_fill_console(self, text):
+        ut._set_text_item_text(self.txtConsole, text)
+
+    def replace_last_console_line(self, text_line):
+        ut._replace_text_item_last_line(self.txtConsole, text_line)
---
+++ utils.py
@@ -0,0 +1,72 @@
+import os
+import subprocess
+from threading import Thread
+
+from tkinter import END
+from tkinter import Text
+
+START = "1.0" #start index for text item
+LAST_LINE = ("end-1l", END)
+PENULTIMATE_LINE = ("end-2l", "end-1l")
+
+youtube_dl_app = "C:\\Program Files (x86)\\youtube-dl\\youtube-dl.exe"
+
+#gui utils
+def _post_format(text_item: Text, text_line, contains=None):
+    contains = contains if contains else {}
+    for sub_str, color in contains.items():
+        if _check_substrings(text_line, sub_str):
+            text_item.tag_add(sub_str, *PENULTIMATE_LINE)
+            text_item.tag_config(sub_str, foreground=color)
+
+def _check_substrings(inspected_str: str, subs):
+    subs = subs.split("+")
+    return all( [(s in inspected_str) for s in subs] )
+
+def _append_text_item_text_line(text_item: Text, text_line: str):
+    text_item.insert(END, text_line)
+
+def _replace_text_item_last_line(text_item: Text, text_line: str):
+    text_item.delete(*LAST_LINE)
+    text_line = "\n" + text_line
+    text_item.insert(END, text_line)
+
+def _set_text_item_text(text_item: Text, text: str):
+    text_item.delete(START, END)
+    text_item.insert(START, text)
+
+def exec_youtube_dl(link, *options) -> subprocess.Popen:
+    l = list(options)
+    l.insert(0, youtube_dl_app)
+    l.append(link)
+    return subprocess.Popen(l, stdout=subprocess.PIPE)
+
+def exec_get_info(link) -> subprocess.Popen:
+    return exec_youtube_dl(link, "-F")
+
+#utils
+def _readline(obj, newline = (b"\n", b"\r")):
+    l = []
+    while True:
+        c = obj.read(1)
+        if c:
+            l.append(c)
+        if not c or c in newline:
+            break
+    return b"".join(l)
+
+def read_output(p: subprocess.Popen, out_append = print, out_replace = print):
+    proc_stdout = p.stdout
+    while True:
+        try:
+            text = _readline(proc_stdout)
+            if text:
+                text = text.decode("cp1251")
+                if text.endswith("\r"):
+                    out_replace(text)
+                elif text.endswith("\n"):
+                    out_append(text)
+            else:
+                break
+        except ValueError:
+            break
